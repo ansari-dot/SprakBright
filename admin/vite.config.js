9a05ade242a69ed8204ef4abf57ed3ccf2d62120
ansari-dot <0349ansari@gmail.com>
2025-11-25 02:27:03 +0500

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,25 +1,28 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const url = env.VITE_BACKEND_URL
+  const apiUrl = env.VITE_BACKEND_URL
 
   return {
-    base: mode === 'production' ? '/' : '/', // change '/' to '/admin/' if deployed in a subfolder
+    base: mode === 'production' ? '/' : '/', // change '/' to '/admin/' if needed
     plugins: [react()],
     server: {
       proxy: {
         '/api/uploads': {
-          target: url,
+          target: apiUrl,
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/api\/uploads/, '/uploads'),
         },
         '/api': {
-          target: url,
+          target: apiUrl,
           changeOrigin: true,
           secure: false,
         },
         '/uploads': {
-          target: url,
+          target: apiUrl,
           changeOrigin: true,
           secure: false,
         },
